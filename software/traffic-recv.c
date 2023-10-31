47ba4b13a19137e20b97cf13f4acae2253f4b398
joey0320 <joonho0320@gmail.com>
2023-10-30 22:24:49 -0700

    Stack -> Queue

--- software/traffic-recv.c
+++ software/traffic-recv.c
@@ -13,16 +13,34 @@
 /* #define NO_NET_DEBUG */
 #define TRAFFIC_RECV_DEBUG
 
-#define N_CORES 8
+#define N_CORES 12
 #define RUN_CORES 2
 #define TX_DESC_CNT 128
 #define RX_DESC_CNT 128
 #define PACKET_BYTES 1500
 #define PACKET_BYTES_PADDED (PACKET_BYTES+8)
-#define MAX_PACKETS_TO_FORWARD 1000000
+#define MAX_PACKETS_TO_FORWARD (50000 / RUN_CORES)
 
 
-void forward_traffic(int core_id, Stack* rx, uint8_t** rx_desc, Stack* tx, uint8_t** tx_desc) {
+void forward_traffic(int core_id) {
+  Queue* rx_idx;
+  uint8_t* rx_desc[RX_DESC_CNT];
+  Queue* tx_idx;
+  uint8_t* tx_desc[TX_DESC_CNT];
+
+  // setup rx desc
+  rx_idx = newQueue(RX_DESC_CNT);
+  for (int i = 0; i < RX_DESC_CNT; i++) {
+    rx_desc[i] = (uint8_t*)malloc(sizeof(uint8_t) * PACKET_BYTES_PADDED);
+    enqueue(rx_idx, i);
+  }
+
+  // setup tx desc
+  tx_idx = newQueue(TX_DESC_CNT);
+  for (int i = 0; i < TX_DESC_CNT; i++) {
+    tx_desc[i] = (uint8_t*)malloc(sizeof(uint8_t) * PACKET_BYTES_PADDED);
+    enqueue(tx_idx, i);
+  }
 
   acquire_lock();
   fprintf(stdout, "Core %d start forwarding traffic\n", core_id);
@@ -30,10 +48,10 @@ void forward_traffic(int core_id, Stack* rx, uint8_t** rx_desc, Stack* tx, uint8
 
   Queue* inflight_rx = newQueue(RX_DESC_CNT);
 
-  int cnt = 0;
+  int cnt = 0, prev_cnt = 0;
   int recv_avail, send_avail;
   do {
-    int rid, tid, h;
+    int rid, tid;
 
     // Push in as much rx descriptors as it can
 #ifndef NO_NET_DEBUG
@@ -43,27 +61,35 @@ void forward_traffic(int core_id, Stack* rx, uint8_t** rx_desc, Stack* tx, uint8
     recv_avail = RX_DESC_CNT;
 #endif
     for (int i = 0; i < recv_avail; i++) {
-      acquire_lock();
-      h = height(rx);
-      if (h > 0) {
-        rid = pop(rx);
+      if (!isempty(rx_idx)) {
+        rid = dequeue(rx_idx);
         enqueue(inflight_rx, rid);
-/* printf("core: %d rid: %d inflight: %d\n", core_id, rid, size(inflight_rx)); */
 #ifndef NO_NET_DEBUG
         nic_set_recv_addr(core_id, (uint64_t)rx_desc[rid]);
 #endif
       }
-      release_lock();
     }
 
-    acquire_lock();
-    printf("core: %d inflight: %d cnt: %d\n", core_id, size(inflight_rx), cnt);
-    release_lock();
+    if (cnt - prev_cnt > 1000) {
+      acquire_lock();
+      printf("core: %d inflight: %d cnt: %d\n", core_id, size(inflight_rx), cnt);
+#ifndef NO_NET_DEBUG
+      if (core_id == 0) {
+        printf("rd: %d wr: %d\n", (int)nic_ddio_rd_avg_lat(N_CORES), (int)nic_ddio_wr_avg_lat(N_CORES));
+      }
+#endif
+      prev_cnt = cnt;
+      release_lock();
+    }
 
     // Receive packets & forward it
 #ifndef NO_NET_DEBUG
     int recv_comps = nic_recv_comp_avail(core_id);
     asm volatile("fence");
+/* acquire_lock(); */
+/* printf("recv_comps: %d, inflight_req.size: %d\n", recv_comps, size(inflight_rx)); */
+/* release_lock(); */
+    assert(recv_comps <= size(inflight_rx));
 #else
     int recv_comps = size(inflight_rx);
 #endif
@@ -72,23 +98,17 @@ void forward_traffic(int core_id, Stack* rx, uint8_t** rx_desc, Stack* tx, uint8
 #ifndef NO_NET_DEBUG
       uint64_t len = nic_recv_comp(core_id);
 #endif
-      assert(!isempty(inflight_rx));
-      rid = dequeue(inflight_rx);
 
       // Copy the rx descriptor to the tx descriptor
-      acquire_lock();
-      assert(!stackfull(rx));
-      push(rx, rid);
+      assert(!isempty(inflight_rx));
+      assert(space(rx_idx) > 0);
+      assert(!isempty(tx_idx));
 
-      assert(height(tx) > 0);
-      tid = pop(tx);
+      rid = dequeue(inflight_rx);
+      enqueue(rx_idx, rid);
 
-#ifdef TRAFFIC_RECV_DEBUG
-/* printf("fwd core: %d rid: %d tid: %d\n", core_id, rid, tid); */
-#endif
-
+      tid = dequeue(tx_idx);
       memcpy((void*)tx_desc[tid], (void*)rx_desc[rid], sizeof(uint8_t)*PACKET_BYTES);
-      release_lock();
 
       // Send the tx descriptor
 #ifndef NO_NET_DEBUG
@@ -96,10 +116,7 @@ void forward_traffic(int core_id, Stack* rx, uint8_t** rx_desc, Stack* tx, uint8
 #endif
 
       // release the used tx descriptor
-      acquire_lock();
-      push(tx, tid);
-      release_lock();
-
+      enqueue(tx_idx, tid);
       cnt++;
     }
   } while (cnt < MAX_PACKETS_TO_FORWARD);
@@ -110,37 +127,10 @@ void __main(void) {
   size_t mhartid = read_csr(mhartid);
   if (mhartid >= RUN_CORES) while (1);
 
-  static volatile int setup_done;
-  static Stack* rx_idx;
-  static uint8_t* rx_desc[RX_DESC_CNT];
-  static Stack* tx_idx;
-  static uint8_t* tx_desc[TX_DESC_CNT];
-  if (mhartid == 0) {
-    // setup rx desc
-    rx_idx = newStack(RX_DESC_CNT);
-    for (int i = 0; i < RX_DESC_CNT; i++) {
-      rx_desc[i] = (uint8_t*)malloc(sizeof(uint8_t) * PACKET_BYTES_PADDED);
-      push(rx_idx, RX_DESC_CNT - i - 1);
-    }
-
-    // setup tx desc
-    tx_idx = newStack(TX_DESC_CNT);
-    for (int i = 0; i < TX_DESC_CNT; i++) {
-      tx_desc[i] = (uint8_t*)malloc(sizeof(uint8_t) * PACKET_BYTES_PADDED);
-      push(tx_idx, TX_DESC_CNT - i - 1);
-    }
-
-    // setup complete
-    setup_done = 1;
-  } else {
-    // spin until core 0 finishes nic descriptor setup
-    while (!setup_done)
-      ;
-  }
 
   for (int i = 0; i < RUN_CORES; i++) {
     if (mhartid == i) {
-      forward_traffic(mhartid, rx_idx, &rx_desc, tx_idx, &tx_desc);
+      forward_traffic(mhartid);
     }
   }
 
